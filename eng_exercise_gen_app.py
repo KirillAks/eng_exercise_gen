fcd141fdc0ed1b73be95f28a88d7d9cbc20d8d2c
KirillAks <95132232+KirillAks@users.noreply.github.com>
2023-07-12 15:56:06 +0300

    Add files via upload

--- eng_exercise_gen_app.py
+++ eng_exercise_gen_app.py
@@ -29,61 +29,56 @@ if uploaded_file is not None:
 #     st.write("Повторите текст")
 # else:
 #     st.write('Пожалуйста, подождите, пока сгенерируются упражнения...')
-
-
-# if st.button('Сгенерировать упражнения'):
-#     st.write('Пожалуйста, подождите, пока сгенерируются упражнения...')
-#     df = eeg.create_sentence('Little_Red_Cap_ Jacob_and_Wilhelm_Grimm.txt')    
-#     tasks = eeg.create_df(df)
     
-if 'button' not in st.session_state:
-    st.session_state.button = False
+# if 'button' not in st.session_state:
+#     st.session_state.button = False
 
-def click_button():
-    st.session_state.button = not st.session_state.button
+# def click_button():
+#     st.session_state.button = not st.session_state.button
 
-st.button('Сгенерировать упражнения', on_click=click_button)
+# st.button('Сгенерировать упражнения', on_click=click_button)
 
-if st.session_state.button:
-    st.write('Пожалуйста, подождите, пока сгенерируются упражнения...')
-    df = eeg.create_sentence('Little_Red_Cap_ Jacob_and_Wilhelm_Grimm.txt')
-    tasks = eeg.create_df(df) 
-    '---'
-    for i, row in tasks.iterrows():
-        st.subheader(row['description']) 
+# if st.session_state.button:
+#     st.write('Пожалуйста, подождите, пока сгенерируются упражнения...')
+#     df = eeg.create_sentence('Little_Red_Cap_ Jacob_and_Wilhelm_Grimm.txt')
+#     tasks = eeg.create_df(df) 
 
-        col1, col2 = st.columns(2)
-        with col1:
-            st.write('')
-            st.write(row['sent_ex'])
+df = eeg.create_sentence('Little_Red_Cap_ Jacob_and_Wilhelm_Grimm.txt')
+tasks = eeg.create_df(df)
+'---'
+for i, row in tasks.iterrows():
+    st.subheader(row['description']) 
 
-        with col2:
-            option = row['options']
-            if row['type']=='missing_word':
-                text = '–––'
-                row['result'] = st.text_area("Напишите ответ:", text, key=f"{i+20}")
-            else:
-                row['result'] = st.selectbox(
-                    'nolabel',
-                    ['–––'] + option,
-                    key = f"{i}",
-                    label_visibility="hidden",
-                ) 
-            if row['result'] == '–––':
-                pass
-            elif row['result'] == row['answer']:
-                st.success('Correctly', icon="💪")
-            else:
-                st.error('Mistake', icon="🤷‍♂️")
+    col1, col2 = st.columns(2)
+    with col1:
+        st.write('')
+        st.write(row['sent_ex'])
 
-#         tasks['total'] = row['result'] == row['answer']
-        '---'    
+    with col2:
+        option = row['options']
+        if row['type']=='missing_word':
+            text = '–––'
+            row['result'] = st.text_area("Напишите ответ:", text, key=f"{i+20}")
+        else:
+            row['result'] = st.selectbox(
+                'nolabel',
+                ['–––'] + option,
+                key = f"{i}",
+                label_visibility="hidden",
+            ) 
+        if row['result'] == '–––':
+            pass
+        elif row['result'] == row['answer']:
+            st.success('Correctly', icon="💪")
+        else:
+            st.error('Mistake', icon="🤷‍♂️")
 
-#     total_sum = sum(tasks['total'])
+    tasks['total'] = row['result'] == row['answer']
+    '---'    
 
-#     if total_sum == len(tasks):
-#         st.success('Поздравляем! Вы ответили на все вопросы!')
-#         st.balloons()
-else:
-    st.write('Button is off!')    
+total_sum = sum(tasks['total'])
+
+if total_sum == len(tasks):
+    st.success('Поздравляем! Вы ответили на все вопросы!')
+    st.balloons()
 
