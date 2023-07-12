0d1b82634f13260d8539b5b96fb6a416c40f810a
KirillAks <95132232+KirillAks@users.noreply.github.com>
2023-07-12 09:01:20 +0300

    Add files via upload

--- eng_exercise_gen_app.py
+++ eng_exercise_gen_app.py
@@ -19,52 +19,47 @@ if uploaded_file is not None:
     string_data = stringio.read()
     st.write(string_data)
 
-# if st.button('Сгенерировать упражнения'):
-#     st.write('Пожалуйста, подождите, пока сгенерируются упражнения...')
-#     df = eeg.create_sentence('Little_Red_Cap_ Jacob_and_Wilhelm_Grimm.txt')
-#     st.write(df)
-#     tasks = eeg.create_df(df)
-#     st.write(tasks)
+if st.button('Сгенерировать упражнения'):
+    st.write('Пожалуйста, подождите, пока сгенерируются упражнения...')
+    df = eeg.create_sentence('Little_Red_Cap_ Jacob_and_Wilhelm_Grimm.txt')    
+    tasks = eeg.create_df(df)
 
-df = eeg.create_sentence('Little_Red_Cap_ Jacob_and_Wilhelm_Grimm.txt')
-tasks = eeg.create_df(df)
-'---'
-st.header('Упражнения по английскому')
-'---'
-for i, row in tasks.iterrows():
-    st.subheader(row['description']) 
+    '---'
+    st.header('Упражнения по английскому')
+    '---'
+    for i, row in tasks.iterrows():
+        st.subheader(row['description']) 
 
-    col1, col2 = st.columns(2)
-    with col1:
-        st.write('')
-        st.write(row['sent_ex'])
+        col1, col2 = st.columns(2)
+        with col1:
+            st.write('')
+            st.write(row['sent_ex'])
 
-    with col2:
-        option = row['options']
-        if row['type']=='missing_word':
-            text = '–––'
-            row['result'] = st.text_area("Напишите ответ:", text, key=f"{i+20}")
-        else:
-            row['result'] = st.selectbox(
-                'nolabel',
-                ['–––'] + option,
-                key = f"{i}",
-                label_visibility="hidden",
-            ) 
-        if row['result'] == '–––':
-            pass
-        elif row['result'] == row['answer']:
-            st.success('Correctly', icon="💪")
-        else:
-            st.error('Mistake', icon="🤷‍♂️")
+        with col2:
+            option = row['options']
+            if row['type']=='missing_word':
+                text = '–––'
+                row['result'] = st.text_area("Напишите ответ:", text, key=f"{i+20}")
+            else:
+                row['result'] = st.selectbox(
+                    'nolabel',
+                    ['–––'] + option,
+                    key = f"{i}",
+                    label_visibility="hidden",
+                ) 
+            if row['result'] == '–––':
+                pass
+            elif row['result'] == row['answer']:
+                st.success('Correctly', icon="💪")
+            else:
+                st.error('Mistake', icon="🤷‍♂️")
 
-#     tasks['total'] = row['result'] == row['answer']
-    '---'    
+        tasks['total'] = row['result'] == row['answer']
+        '---'    
 
-# total_sum = sum(tasks['total'])
+    total_sum = sum(tasks['total'])
 
-# if total_sum == len(tasks):
-#     st.success('Поздравляем! Вы ответили на все вопросы!')
-#     st.balloons()
-    
+    if total_sum == len(tasks):
+        st.success('Поздравляем! Вы ответили на все вопросы!')
+        st.balloons()
 
